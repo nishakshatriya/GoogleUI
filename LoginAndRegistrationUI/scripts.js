a443ffb869e654b6826e6f45f20360893a1d605e
nishakshatriya <nisha.kshatriya08@gmail.com>
2020-08-31 08:55:59 +0530

    [Feedback] Refactor: All the changes during feedback

--- LoginAndRegistrationUI/scripts.js
+++ LoginAndRegistrationUI/scripts.js
@@ -1,25 +1,33 @@
 function validation(){
-    var username = document.getElementsByName('username')[0].value;
-    var email = document.getElementsByName('useremail')[0].value;
-    var pswd = document.getElementsByName('userpassword')[0].value;
-
+    var fname = document.getElementsByClassName('first-name')[0].value;
+    var lname = document.getElementsByClassName('last-name')[0].value;
+    var email = document.getElementsByClassName('useremail')[0].value;
+    var pswd = document.getElementsByClassName('user-password')[0].value;
+    var pswdd = document.getElementsByClassName('confirm-password')[0].value;
     var usernamecheck = /^[A-za-z.]{3,30}$/;
     var userpswdcheck = /(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z])(?=.*[+@#$%^&*!.()-]).{8,}/;
     var emailcheck = /^[a-zA-Z0-9]{1,}[._+-]?[a-zA-Z0-9]{1,}[@][a-zA-Z0-9]{1,}([.][a-zA-Z]{2,4}){1,3}$/;
     
     var counter=0;
-    if(usernamecheck.test(username)){
+    if(usernamecheck.test(fname)){
+        document.getElementById('name-error').innerHTML = "";
+    }else{
+        counter++;
+        document.getElementById('name-error').innerHTML = "First Name is invalid";
+    }
+
+    if(usernamecheck.test(lname)){
         document.getElementById('nameerror').innerHTML = "";
     }else{
         counter++;
-        document.getElementById('nameerror').innerHTML = "Username is invalid";
+        document.getElementById('nameerror').innerHTML = "Last Name is invalid";
     }
 
     if(emailcheck.test(email)){
         document.getElementById('emailerror').innerHTML = "";
     }else{
         counter++;
-        document.getElementById('emailerror').innerHTML = "Email is invalid";
+        document.getElementById('emailerror').innerHTML = "Username is invalid";
     }
 
     if(userpswdcheck.test(pswd)){
@@ -27,6 +35,14 @@ function validation(){
     }else{
         counter++;
         document.getElementById('passworderror').innerHTML = "Password is invalid";
+    }
+
+    if(pswd.match(pswdd)){
+        document.getElementById('passworderror').innerHTML = "";
+
+    }else{
+        counter++;
+        document.getElementById('passworderror').innerHTML = "Password does'nt match!"
     }
     if(counter > 0)
     {
@@ -39,16 +55,6 @@ function validation(){
 function togglePass(){ 
     (pwd.type == 'password') ? pwd.type = 'text' : 
     pwd.type = 'password';
-    eye.classList.toggle('active');       
+    eye.classList.toggle('active');      
 }
 
-function disableButton(){
-    if((document.getElementsByName('username')[0].value != "")){
-        if((document.getElementsByName('pwd')[0].value != "")){
-            document.getElementsByName('button')[0].disabled = false;
-        }                   
-    }
-    else{
-        document.getElementsByName("button")[0].disabled = true;
-    }
-}
